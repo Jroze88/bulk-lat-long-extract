aab601a7420c3a27d394f1565a0ca9913a8e4bf0
Supervillain <supervillain@Kallima.OmKreemKalikayaiNamah>
2020-08-10 12:03:43 -0400

    reinit

--- image-lat-long-script.py
+++ image-lat-long-script.py
@@ -1,32 +1,21 @@
-import csv
 import os
-from GPSPhoto import gpsphoto
-import exifread
-
+import csv
+points = []
+names = []
+count = -1 
+# (-1) to make the array iterator match the number instead of making two variables
+for r, d, f in os.walk(path):
+    for file in f:
+        if file.lower().endswith(('.png', '.jpg', '.jpeg', '.tiff', '.mpeg', '.mp4', '.mpg')):
+            filepath = os.path.join(r, file)
+            exif = get_exif(filepath)
+            if exif is not None and 'GPSInfo' in exif:
+                latlong = get_decimal_coordinates(exif['GPSInfo'])
+                names.append(file)
+                count +=1
+                if latlong is not None:
+                    points.append(latlong)
 with open('locations.csv', 'w', newline='') as csvfile:
     spamwriter = csv.writer(csvfile, delimiter=' ',
                             quotechar='|', quoting=csv.QUOTE_MINIMAL)
-
-# points = []
-# for r, d, f in os.walk(path):
-#     for file in f:
-#         if file.lower().endswith(('.png', '.jpg', '.jpeg')):
-#             filepath = os.path.join(r, file)
-#             exif = get_exif(filepath)
-#             if exif is not None and 'GPSInfo' in exif:
-#                 latlong = get_decimal_coordinates(exif['GPSInfo'])
-#                 if latlong is not None:
-#                     points.append(latlong)
-
-
-count = 0
-
-for subdir, dirs, files in os.walk(os.getcwd()):
-    for file in files:
-        if os.path.splitext(file)[-1].lower() == '.jpg' or os.path.splitext(file)[-1].lower() == '.png' or os.path.splitext(file)[-1].lower() == '.tiff' or os.path.splitext(file)[-1].lower() == '.gif' or os.path.splitext(file)[-1].lower() == '.mp4' or os.path.splitext(file)[-1].lower() == '.mpeg' or os.path.splitext(file)[-1].lower() == '.webm':
-        # f = open(os.path.join(subdir, file), 'rb')
-        # tags = exifread.process_file(f)
-        # print get_exif_location(tags)
-            tags = gpsphoto.getGPSData(os.path.join(subdir, file))
-            count +=1
-            spamwriter.write([str(count), file, tags['Lattitude'], tags['Longitude'], tags['Date']])
+    spamwriter.writerow([str(count), file, points[count])
